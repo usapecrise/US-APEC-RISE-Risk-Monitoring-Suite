764b0037ae7cba51ff1c3211abee919beea9f565
usapecrise <usapecrise@gmail.com>
2025-08-26 10:36:17 -0400

    Update upload_hyper_to_tableau.py

--- .github/scripts/upload_hyper_to_tableau.py
+++ .github/scripts/upload_hyper_to_tableau.py
@@ -37,6 +37,7 @@ EXTRACT_NAME_MAP = {
     "Workshop_Master_List.csv": "Workshop Master List",
     "Feedback_Form_Data.csv": "Feedback Form Data",
     "Stakeholder_Reference_List.csv": "Stakeholder Reference List"
+    "Stakeholder_Reference_List_long.csv": ""Stakeholder Reference List Long"
 }
 
 # ── CONVERT CSV TO HYPER ───────────────────────────────
