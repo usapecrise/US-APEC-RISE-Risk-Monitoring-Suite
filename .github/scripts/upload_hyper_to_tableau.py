e6b0c34563b63bc029d2e3fc15d3d5c9684ae52f
usapecrise <usapecrise@gmail.com>
2025-09-01 11:59:11 -0400

    Update upload_hyper_to_tableau.py

--- .github/scripts/upload_hyper_to_tableau.py
+++ .github/scripts/upload_hyper_to_tableau.py
@@ -39,7 +39,8 @@ EXTRACT_NAME_MAP = {
     "Stakeholder_Reference_List.csv": "Stakeholder Reference List",
     "Stakeholder_Reference_List_long.csv": "Stakeholder Reference List Long",
     "Feedback_Form_Data_Long.csv": "Feedback Form Data Long",
-    "word_frequency.csv": "Word Frequency Extract"
+    "word_frequency.csv": "Word Frequency Extract",
+    "word_frequency_detailed.csv": "Word Frequency Detailed Extract"  
 }
 
 # ── CONVERT CSV TO HYPER ───────────────────────────────
